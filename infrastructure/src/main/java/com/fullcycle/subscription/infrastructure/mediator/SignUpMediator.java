e4d7e61bdb1e0f520a65bdb2022f26d100a602d1
Gabriel <gbns.tech@gmail.com>
2024-04-21 21:51:11 -0300

    10.04-implementacao-do-signup-mediator

--- infrastructure/src/main/java/com/fullcycle/subscription/infrastructure/mediator/SignUpMediator.java
+++ infrastructure/src/main/java/com/fullcycle/subscription/infrastructure/mediator/SignUpMediator.java
@@ -1,13 +1,25 @@
 package com.fullcycle.subscription.infrastructure.mediator;
 
+import com.fullcycle.subscription.application.account.CreateAccount;
+import com.fullcycle.subscription.application.account.CreateIdpUser;
 import com.fullcycle.subscription.infrastructure.rest.models.req.SignUpRequest;
 import com.fullcycle.subscription.infrastructure.rest.models.res.SignUpResponse;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
+
 @Component
 public class SignUpMediator {
 
+    private final CreateAccount createAccount;
+    private final CreateIdpUser createIdpUser;
+
+    public SignUpMediator(final CreateAccount createAccount, final CreateIdpUser createIdpUser) {
+        this.createAccount = Objects.requireNonNull(createAccount);
+        this.createIdpUser = Objects.requireNonNull(createIdpUser);
+    }
+
     public SignUpResponse signUp(final SignUpRequest req) {
-        return null;
+        return this.createAccount.execute(this.createIdpUser.execute(req, req::withUserId), SignUpResponse::new);
     }
 }
