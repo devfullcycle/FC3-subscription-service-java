b90e00248feecdaadd4a80e2f623a345f35225d6
Gabriel <gbns.tech@gmail.com>
2024-04-28 11:20:08 -0300

    14.02-tdd-e-implementacao-do-metodo-save

--- infrastructure/src/main/java/com/fullcycle/subscription/infrastructure/gateway/repository/PlanJdbcRepository.java
+++ infrastructure/src/main/java/com/fullcycle/subscription/infrastructure/gateway/repository/PlanJdbcRepository.java
@@ -6,14 +6,10 @@ import com.fullcycle.subscription.domain.plan.PlanGateway;
 import com.fullcycle.subscription.domain.plan.PlanId;
 import com.fullcycle.subscription.infrastructure.jdbc.DatabaseClient;
 import com.fullcycle.subscription.infrastructure.jdbc.RowMap;
-import org.springframework.security.access.method.P;
 import org.springframework.stereotype.Repository;
 
 import java.time.Instant;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 
 @Repository
 public class PlanJdbcRepository implements PlanGateway {
@@ -46,8 +42,61 @@ public class PlanJdbcRepository implements PlanGateway {
     }
 
     @Override
-    public Plan save(Plan plan) {
-        return null;
+    public Plan save(final Plan plan) {
+        if (plan.version() == 0) {
+            create(plan);
+        } else {
+            update(plan);
+        }
+
+        return plan;
+    }
+
+    private void create(final Plan plan) {
+        final var sql = """
+                INSERT INTO plans (id, version, name, description, active, currency, amount, created_at, updated_at, deleted_at)
+                VALUES (:id, (:version + 1), :name, :description, :active, :currency, :amount, :createdAt, :updatedAt, :deletedAt)
+                """;
+        executeUpdate(sql, plan);
+    }
+
+    private void update(final Plan plan) {
+        final var sql = """
+                UPDATE plans
+                SET
+                    version = (:version + 1),
+                    name = :name,
+                    description = :description,
+                    active = :active,
+                    currency = :currency,
+                    amount = :amount,
+                    created_at = :createdAt,
+                    updated_at = :updatedAt,
+                    deleted_at = :deletedAt
+                WHERE id = :id and version = :version
+                """;
+        if (executeUpdate(sql, plan) == 0) {
+            throw new IllegalArgumentException("Plan with id %s and version %s was not found".formatted(plan.id().value(), plan.version()));
+        }
+    }
+
+    private int executeUpdate(String sql, Plan plan) {
+        final var params = new HashMap<String, Object>();
+        params.put("version", plan.version());
+        params.put("name", plan.name());
+        params.put("description", plan.description());
+        params.put("active", plan.active());
+        params.put("currency", plan.price().currency().getCurrencyCode());
+        params.put("amount", plan.price().amount());
+        params.put("createdAt", plan.createdAt());
+        params.put("updatedAt", plan.updatedAt());
+        params.put("deletedAt", plan.deletedAt());
+
+        if (!Objects.equals(plan.id(), PlanId.empty())) {
+            params.put("id", plan.id().value());
+        }
+
+        return this.database.update(sql, params);
     }
 
     private static RowMap<Plan> planMapper() {
--- infrastructure/src/test/java/com/fullcycle/subscription/infrastructure/gateway/repository/PlanJdbcRepositoryTest.java
+++ infrastructure/src/test/java/com/fullcycle/subscription/infrastructure/gateway/repository/PlanJdbcRepositoryTest.java
@@ -2,6 +2,7 @@ package com.fullcycle.subscription.infrastructure.gateway.repository;
 
 import com.fullcycle.subscription.AbstractRepositoryTest;
 import com.fullcycle.subscription.domain.money.Money;
+import com.fullcycle.subscription.domain.plan.Plan;
 import com.fullcycle.subscription.domain.plan.PlanId;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,8 +10,7 @@ import org.springframework.test.context.jdbc.Sql;
 
 import java.time.Instant;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class PlanJdbcRepositoryTest extends AbstractRepositoryTest {
 
@@ -36,17 +36,117 @@ class PlanJdbcRepositoryTest extends AbstractRepositoryTest {
         var expectedDeletedAt = Instant.parse("2024-04-28T10:59:11.111Z");
 
         // when
-        var actualAccount = this.planRepository().planOfId(expectedId).get();
+        var actualPlan = this.planRepository().planOfId(expectedId).get();
 
         // then
-        assertEquals(expectedId, actualAccount.id());
-        assertEquals(expectedVersion, actualAccount.version());
-        assertEquals(expectedName, actualAccount.name());
-        assertEquals(expectedDescription, actualAccount.description());
-        assertEquals(expectedActive, actualAccount.active());
-        assertEquals(expectedPrice, actualAccount.price());
-        assertEquals(expectedCreatedAt, actualAccount.createdAt());
-        assertEquals(expectedUpdatedAt, actualAccount.updatedAt());
-        assertEquals(expectedDeletedAt, actualAccount.deletedAt());
+        assertEquals(expectedId, actualPlan.id());
+        assertEquals(expectedVersion, actualPlan.version());
+        assertEquals(expectedName, actualPlan.name());
+        assertEquals(expectedDescription, actualPlan.description());
+        assertEquals(expectedActive, actualPlan.active());
+        assertEquals(expectedPrice, actualPlan.price());
+        assertEquals(expectedCreatedAt, actualPlan.createdAt());
+        assertEquals(expectedUpdatedAt, actualPlan.updatedAt());
+        assertEquals(expectedDeletedAt, actualPlan.deletedAt());
+    }
+
+    @Test
+    public void givenEmptyTable_whenInsertSuccessfully_shouldBePersisted() {
+        // given
+        Assertions.assertEquals(0, countPlans());
+
+        var expectedId = new PlanId(1L);
+        var expectedVersion = 1;
+        var expectedName = "Master";
+        var expectedDescription = "O plano mais custo benefício";
+        var expectedActive = false;
+        var expectedPrice = new Money("BRL", 20.0);
+        var expectedCreatedAt = Instant.parse("2024-04-28T10:57:11.111Z");
+        var expectedUpdatedAt = Instant.parse("2024-04-28T10:58:11.111Z");
+        var expectedDeletedAt = Instant.parse("2024-04-28T10:59:11.111Z");
+
+        var plan = Plan.with(expectedId, 0, expectedName, expectedDescription, expectedActive, expectedPrice, expectedCreatedAt, expectedUpdatedAt, expectedDeletedAt);
+
+        // when
+        var responsePlan = this.planRepository().save(plan);
+
+        assertEquals(expectedId, responsePlan.id());
+        assertEquals(0, responsePlan.version());
+        assertEquals(expectedName, responsePlan.name());
+        assertEquals(expectedDescription, responsePlan.description());
+        assertEquals(expectedActive, responsePlan.active());
+        assertEquals(expectedPrice, responsePlan.price());
+        assertEquals(expectedCreatedAt, responsePlan.createdAt());
+        assertEquals(expectedUpdatedAt, responsePlan.updatedAt());
+        assertEquals(expectedDeletedAt, responsePlan.deletedAt());
+
+        // then
+        var actualPlan = this.planRepository().planOfId(expectedId).get();
+
+        assertEquals(expectedId, actualPlan.id());
+        assertEquals(expectedVersion, actualPlan.version());
+        assertEquals(expectedName, actualPlan.name());
+        assertEquals(expectedDescription, actualPlan.description());
+        assertEquals(expectedActive, actualPlan.active());
+        assertEquals(expectedPrice, actualPlan.price());
+        assertEquals(expectedCreatedAt, actualPlan.createdAt());
+        assertEquals(expectedUpdatedAt, actualPlan.updatedAt());
+        assertEquals(expectedDeletedAt, actualPlan.deletedAt());
+    }
+
+    @Test
+    @Sql({"classpath:/sql/plans/seed-plan-master.sql"})
+    public void givenPersistedPlan_whenUpdateSuccessfully_shouldBePersisted() {
+        // given
+        Assertions.assertEquals(1, countPlans());
+
+        var expectedId = new PlanId(1L);
+
+        var persistedPlan = this.planRepository().planOfId(expectedId).get();
+        assertEquals(1, persistedPlan.version());
+        assertEquals("Master", persistedPlan.name());
+        assertEquals("O plano mais custo benefício", persistedPlan.description());
+        assertFalse(persistedPlan.active());
+        assertEquals(new Money("BRL", 20.0), persistedPlan.price());
+        assertEquals(Instant.parse("2024-04-28T10:57:11.111Z"), persistedPlan.createdAt());
+        assertEquals(Instant.parse("2024-04-28T10:58:11.111Z"), persistedPlan.updatedAt());
+        assertEquals(Instant.parse("2024-04-28T10:59:11.111Z"), persistedPlan.deletedAt());
+
+        var expectedVersion = 2;
+        var expectedName = "Plus";
+        var expectedDescription = "O plano PLUS";
+        var expectedActive = true;
+        var expectedPrice = new Money("USD", 5.0);
+        var expectedCreatedAt = Instant.parse("2024-04-27T10:57:11.111Z");
+        var expectedUpdatedAt = Instant.parse("2024-04-27T10:58:11.111Z");
+        var expectedDeletedAt = Instant.parse("2024-04-27T10:59:11.111Z");
+
+        var plan = Plan.with(expectedId, 1, expectedName, expectedDescription, expectedActive, expectedPrice, expectedCreatedAt, expectedUpdatedAt, expectedDeletedAt);
+
+        // when
+        var responsePlan = this.planRepository().save(plan);
+
+        assertEquals(expectedId, responsePlan.id());
+        assertEquals(1, responsePlan.version());
+        assertEquals(expectedName, responsePlan.name());
+        assertEquals(expectedDescription, responsePlan.description());
+        assertEquals(expectedActive, responsePlan.active());
+        assertEquals(expectedPrice, responsePlan.price());
+        assertEquals(expectedCreatedAt, responsePlan.createdAt());
+        assertEquals(expectedUpdatedAt, responsePlan.updatedAt());
+        assertEquals(expectedDeletedAt, responsePlan.deletedAt());
+
+        // then
+        var actualPlan = this.planRepository().planOfId(expectedId).get();
+
+        assertEquals(expectedId, actualPlan.id());
+        assertEquals(expectedVersion, actualPlan.version());
+        assertEquals(expectedName, actualPlan.name());
+        assertEquals(expectedDescription, actualPlan.description());
+        assertEquals(expectedActive, actualPlan.active());
+        assertEquals(expectedPrice, actualPlan.price());
+        assertEquals(expectedCreatedAt, actualPlan.createdAt());
+        assertEquals(expectedUpdatedAt, actualPlan.updatedAt());
+        assertEquals(expectedDeletedAt, actualPlan.deletedAt());
     }
 }
