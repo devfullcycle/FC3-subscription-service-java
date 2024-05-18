e1c277f4947d55971b03306576f1c3124d9409b0
Gabriel <gbns.tech@gmail.com>
2024-05-18 13:33:11 -0300

    16.05-implementando-paymentgateway-mock-e-finalizando-teste-de-regressao

--- infrastructure/src/main/java/com/fullcycle/subscription/infrastructure/rest/models/res/ChargeSubscriptionResponse.java
+++ infrastructure/src/main/java/com/fullcycle/subscription/infrastructure/rest/models/res/ChargeSubscriptionResponse.java
@@ -15,8 +15,8 @@ public record ChargeSubscriptionResponse(
                 out.subscriptionId().value(),
                 out.subscriptionStatus(),
                 out.subscriptionDueDate().toString(),
-                out.paymentTransaction().transactionId(),
-                out.paymentTransaction().errorMessage()
+                out.paymentTransaction() == null ? null : out.paymentTransaction().transactionId(),
+                out.paymentTransaction() == null ? null : out.paymentTransaction().errorMessage()
         );
     }
 }
